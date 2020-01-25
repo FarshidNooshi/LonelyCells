d5c974cba48736c95d54ee67ff4e2cda7851ed6b
FarshidNooshi@users.noreply.github.com <FarshidNooshi@users.noreply.github.com>
2020-01-26 00:40:56 +0330

    now i can print a simple honeycomb

--- MapEditor.h
+++ MapEditor.h
@@ -1,7 +1,58 @@
-#ifndef MapEditor
-    void MyPrint() {
-        printf("!");
-        return ;
+//In The Name Of GOD
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define Z   40
+
+int32_t main() {
+    int n;  cin >> n;
+    string st, ts;
+    for (int i = 0; i < n - 1; i++)
+        st += '*', ts += ' ';
+    st += ' ';
+    ts += ' ';
+    for (int i  = 0; i < Z; i++) {
+        for (int j = 0; j < Z; j++) {
+            int w = i % (2 * n - 2), x = j % (4 * n - 4);
+            if (w < n) {
+                if (w == n - 1) {
+                    if (!j) 
+                        cout << st;
+                    else {
+                        if (j < 3 * n - 3) {
+                            cout << ts;
+                            continue;
+                        }
+                        int y = j - (2 * n - 2);
+                        x = y % (4 * n - 4);
+                        if (x >= n - 1 && x <= 2 * n - 2)
+                            cout << st;
+                        else 
+                            cout << ts;
+                    }
+                } else {
+                    if (!w) {
+                        if (x >= n - 1 && x <= 2 * n - 2)
+                            cout << st;
+                        else 
+                            cout << ts;
+                    } else {
+                        if (x == n - 1 - w || x == 2 * n - 2 + w)
+                            cout << st;
+                        else 
+                            cout << ts;
+                    }
+                }
+            } else {
+                int y = w - (n - 1);
+                w = n - 1 - y;
+                if (x == n - 1 - w || x == 2 * n - 2 + w)
+                    cout << st;
+                else 
+                    cout << ts;
+            }
+        }
+        printf("\n");
     }
-    #define MapEditor
-#endif
+}
--- main.cpp
+++ main.cpp
@@ -15,14 +15,10 @@ typedef struct table    stable;
 
 enum blocks { ENERGY = 1, MITOSIS, FORBIDDEN, NORMAL };
 
-struct String {
-    char s[MAXN];
-    int sz;
-};
-
 struct Cell {
     int x, y;
-    String name;
+    char* s;
+    size_t sz;
     sCell* nxt;
 };
 
@@ -30,24 +26,96 @@ struct table {
     int state;
 };
 
-sCell* lst;//سلول ها 
+sCell* lst[2];//سلول ها 
 stable Tbl[MAXN][MAXN];
+bool mark[MAXN][MAXN];
 int n;
 FILE* f1;
 
-void MyRead();
+void CreateNew(sCell**);
+void MyRead(int );
+void RunSingleMode();
+void RunMultiMode();
+void init(sCell* , int);
+void BuildCells(int);
 
 int main() {
-    MyRead();
+    srand(time(NULL));
+    MyRead(0);
+    printf("[1]Load\n");
+    printf("[2]New Single player game\n");
+    printf("[3]New Multiplayer game\n");
+    printf("[4]Exit\n");
+    int tmp;    scanf("%d", &tmp);
+    if (tmp == 1) 
+        MyRead(1);
+    else if (tmp == 2) 
+        RunSingleMode();
+    else if (tmp == 3) 
+        RunMultiMode();
+    return 0;
 }
 
-void MyRead() {
+void CreateNew(sCell** mem) {
+    sCell* t = *mem;
+    sCell* tmp = (sCell*)malloc(sizeof(sCell));
+    tmp->nxt = NULL;
+    t->nxt = tmp;
+}
+
+void BuildCells(int t) {
+    int m;  scanf("%d", &m);
+    sCell* ptr = lst[t];
+    size_t len = 5;
+    for (int i = 0; i < m; i++) {
+        CreateNew(&ptr);
+        init(ptr, len);
+        ptr->x = rand() % n, ptr->y = rand() % n;
+        while (mark[ptr->x][ptr->y])
+            ptr->x = rand() % n, ptr->y = rand() % n;
+        mark[ptr->x][ptr->y] = 1;
+    }
+}
+
+void RunSingleMode() {
+    BuildCells(0);
+}
+
+void RunMultiMode() {
+    BuildCells(0);
+    BuildCells(1);
+    printf("Do you want to play against computer?\ny = YES, n = NO\n");
+    char tmp;   scanf("%c", &tmp);
+    int flag = 0;
+    while (1) {
+        if (tmp == 'y') {
+            if (flag % 2) 
+                CTurn();
+            else {
+                
+            } 
+
+        }
+    }
+}
+
+void init(sCell* ptr, size_t size) {
+    ptr->s = malloc((size + 1) * sizeof(char));
+    ptr->sz = size;
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (size_t i = 0; i < size; i++) {
+        int key = rand() % (sizeof(charset) / sizeof(char) - 1);
+        ptr->s[i] = charset[key];
+    }
+    ptr->s[size] = '\0';
+}
+
+void MyRead(int flag) {
     f1 = fopen("map.bin", "rb");
     if (fread(&n, sizeof(int), 1, f1) < 1) {
         printf("Error occured\n");
-        return 0;
+        exit(0);
     }
-    printf("%d\n", n);
     for (int i = 0; i < n; i++) {
         char state[MAXN];
         fread(state, sizeof(char), n, f1);
@@ -55,4 +123,7 @@ void MyRead() {
             Tbl[i][j].state = state[j] - '0';
     }
     fclose(f1);
+    if (flag) {
+        ...
+    }
 }
